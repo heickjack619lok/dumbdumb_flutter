ff254589c1a1ca30fd0cd6793fd1cdef0c4d3c01
Loong Yeat <pongloongyeat@gmail.com>
2022-07-20 15:57:22 +0800

    refactor: Use freezed for UserModel

--- lib/app/model/user_model.dart
+++ lib/app/model/user_model.dart
@@ -1,51 +1,22 @@
-class UserModel {
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  UserModel(
-      {this.id,
-        this.gender,
-        this.email,
-        this.fullName,
-        this.createdDate,
-        this.phoneNumber,
-        this.accessToken,
-        this.refreshToken});
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
 
-  UserModel.fromJson(Map<String, dynamic> json) {
-    id = getStringValue(json['id']);
-    email = getStringValue(json['email']);
-    icNumber = getStringValue(json['icNumber']);
-    accessToken = getStringValue(json['auth_token']);
-    refreshToken = getStringValue(json['refresh_token']);
-    fullName = getStringValue(json['fullName']);
-    phoneNumber = getStringValue(json['phoneNumber']);
-    createdDate = getStringValue(json['createdDate']);
-    gender = int.tryParse(json['gender'].toString()) ?? 0;
-  }
+@freezed
+class UserModel with _$UserModel {
+  factory UserModel({
+    String? id,
+    int? gender,
+    String? email,
+    String? icNumber,
+    String? fullName,
+    String? createdDate,
+    String? phoneNumber,
+    String? accessToken,
+    String? refreshToken,
+  }) = _UserModel;
 
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['id'] = id;
-    data['email'] = email;
-    data['gender'] = gender;
-    data['icNumber'] = icNumber;
-    data['fullName'] = fullName;
-    data['auth_token'] = accessToken;
-    data['createdDate'] = createdDate;
-    data['phoneNumber'] = phoneNumber;
-    data['refresh_token'] = refreshToken;
-    return data;
-  }
-
-  String getStringValue(dynamic? jsonValue) =>
-      jsonValue != null ? jsonValue.toString() : '';
-
-  String? id;
-  int? gender;
-  String? email;
-  String? icNumber;
-  String? fullName;
-  String? createdDate;
-  String? phoneNumber;
-  String? accessToken;
-  String? refreshToken;
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
 }
